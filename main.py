2c7f1b5544a5f1d46cf3eb6862ba99d92bd144c8
Khudoteplov <d-n-khudoteplov@yandex.ru>
2020-11-27 12:20:03 +0300

    event handling added

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 import pygame
+
+pygame.init()
+
+pygame.display.init()
+
+screen = pygame.display.set_mode((400, 500))
+
+finished = False
+
+while not finished:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+
+pygame.quit()
