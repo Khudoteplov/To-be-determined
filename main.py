98ed9f0d627bcc78a156850f9c53b519b0310ea0
Khudoteplov <d-n-khudoteplov@yandex.ru>
2020-12-16 22:39:10 +0300

    .

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from classes import *
 from mechanics import *
 # from graphics import *
 from constants import *
+from platform_generator import *
 
 pygame.init()
 
@@ -14,9 +15,21 @@ finished = False
 
 character = Character()
 
-
 clock = pygame.time.Clock()
+
+height = 0
+platforms = []
+top_generated_level = 10 * screen_height
+platforms += generate_platforms(0, 10 * screen_height)
+
 while not finished:
+    move_hero(character, 1)
+    for platform in platforms:
+        if check_bounce(character, platform):
+            bounce(character, platform)
+            height = platform.y
+            break
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
