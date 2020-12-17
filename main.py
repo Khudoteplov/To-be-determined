2185fd9e4a0076c512c45b61cf589c276ab69e30
Khudoteplov <d-n-khudoteplov@yandex.ru>
2020-12-17 10:45:42 +0300

    Added function that updates screen

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
 import pygame
 from classes import *
 from mechanics import *
-# from graphics import *
+from graphics import *
 from constants import *
 from platform_generator import *
+scroll_speed = 20
+
+
+def update_screen(screen1: pygame.Surface,
+                  height1, view_height1, character1, platforms1):
+    """
+    Функция обновляет картинку на экране
+
+    Аргумеенты: **screen1** - поверхность экрана
+    **height1** - высота, до которой дошел игрок
+    **view_height1** - выстота наблюдателя
+    **character1** - персонаж
+    **platforms1** - список объектов класса Platform
+    """
+    screen1.blit(background_surface, (0, 0))  # отрисовка фона
+    for platform1 in platforms1:
+        screen1.blit(platform_surface,
+                     (platform1.x - platform1.width // 2,
+                      -platform1.y + view_height1 + screen_height))
+    screen.blit(character_turned_right_surface,
+                (character1.x - 25,
+                 -character.y + view_height1 + screen_height - 50))
+    if view_height1 < height:
+        view_height1 = min(view_height1 + scroll_speed, height1)
+
+    return view_height1
+
 
 pygame.init()
 
@@ -18,6 +45,7 @@ character = Character()
 clock = pygame.time.Clock()
 
 height = 0
+point_of_view_height = 0
 platforms = []
 top_generated_level = 10 * screen_height
 platforms += generate_platforms(0, 10 * screen_height)
@@ -28,8 +56,12 @@ while not finished:
         if check_bounce(character, platform):
             bounce(character, platform)
             height = platform.y
+            if (top_generated_level - height) < screen_height:
+                generate_platforms(top_generated_level,
+                                   top_generated_level + 5 * screen_height)
+                top_generated_level += 5 * screen_height
             break
-
+    remove_passed_platforms(point_of_view_height, platforms)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
@@ -38,8 +70,8 @@ while not finished:
                 move_hero_left(character)
             elif event.key == pygame.K_RIGHT:
                 move_hero_right(character)
-    # FIXME: update visual
-
+    point_of_view_height = update_screen(screen, height, point_of_view_height,
+                                         character, platforms)
     clock.tick(FPS)
 
 pygame.quit()
