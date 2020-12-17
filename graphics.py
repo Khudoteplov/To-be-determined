56a16a0ee1a94b88679cc559cb031f045671e888
veratsurkis <tsurkis.vi@phystech.edu>
2020-12-17 16:24:56 +0300

    Changed functions using classes

--- graphics.py
+++ graphics.py
@@ -1,34 +1,41 @@
 import pygame
 from pygame.draw import *
 import numpy as np
+from  constant.py import *
+from classes.py import *
 pygame.init()
 
-platform_surface = pygame.Surface((100, 100))
-character_turned_left_surface = pygame.Surface((50, 50)) 
-character_turned_right_surface = pygame.Surface((50, 50))
-background_surface = pygame.Surface((700, 500))
+platform_surface = pygame.Surface((screen_width, screen_height))
+character_turned_left_surface = pygame.Surface((screen_width, screen_height)) 
+character_turned_right_surface = pygame.Surface((screen_width, screen_height))
+background_surface = pygame.Surface((screen_width, screen_height))
 
 BROWN = (73, 50, 36)
 GREEN = (1, 77, 3)
 GREEN_TEA = (208, 240, 192)
 YELLOW = (255, 195, 11)
 
-def platform(Surface, COLOR, x, y, size):
-    rect(Surface, COLOR, (x, y, size*8, size))
+
+def platform(COLOR, platform_pos_x, platform_pos_y, x, y, size):
+    platform = Platform(type, x, y)
+    rect(platform, BROWN, (platform_pos_x, platform_pos_y, platfom_width, platform_height))
+    platform_surface.blit(platform, (x, y))
 
  
-def background(Surface, x, y, size):
-    rect (Surface, GREEN_TEA, (x, y, size_width, size_height))
+def background(x, y, size):
+    rect (background_surface, GREEN_TEA, (x, y, size_width, size_height))
     
     
-def character_turned_left(Surface, x, y, size):
-    ellipse(Surface, YELLOW, (x, y, size, size*3))
-    ellipse(Surface, YELLOW, (x-int(size/2), y+int(size/5), size, size))
-    #not the final version
+def character_turned_left(charecter_pos_x, charecter_pos_y, x, y, size):
+    character_turned_left = Character()
+    ellipse(character_turned_left, YELLOW, (charecter_pos_x, charecter_pos_y, size, size*3))
+    ellipse(character_turned_left, YELLOW, (charecter_pos_x - int(size/2), charecter_pos_y + int(size/5), size, size))
+    character_turned_left_surface.blit(character_turned_left, (x, y))
     
-
-def character_turned_right(Surface, x, y, size):
-    ellipse(Surface, YELLOW, (x, y, size, size*3))
-    ellipse(Surface, YELLOW, (x+size-int(size/2), y+int(size/5), size, size))
-    #not the final version
-  
+    
+def character_turned_right(charecter_pos_x, charecter_pos_y, x, y, size):
+    character_turned_right = Character()
+    ellipse(character_turned_left, YELLOW, (charecter_pos_x, charecter_pos_y, size, size*3))
+    ellipse(character_turned_left, YELLOW, (charecter_pos_x + size - int(size/2), charecter_pos_y + int(size/5), size, size))
+    character_turned_right_surface.blit(character_turned_right, (x, y))
+    
